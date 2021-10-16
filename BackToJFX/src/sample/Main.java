f5f1f997c7ad3a964a26ae57d20b41112d95560d
Isyohan <78814275+Isyohan@users.noreply.github.com>
2021-10-16 17:40:13 +0200

    Just a start

---
+++ BackToJFX/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        Group root=new Group();
+        Pane pane=new Pane(root);
+        Scene theScene=new Scene(pane,600,400,true);
+        primaryStage.setScene(theScene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
